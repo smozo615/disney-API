d9c14be0aef9bc60aaf51a023be78016d20abd27
Santiago Mozo <smozo615@gmail.com>
2022-08-16 02:14:48 -0500

    AUTH: send welcome email

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+// middleware: validator and schemas
+const { dataValidator } = require('../middlewares/validator.middleware');
+const { createUserSchema } = require('./../schemas/user.schema');
+
 // Router
 const router = express.Router();
 
@@ -33,14 +37,58 @@ const service = new AuthService();
  *      '400':
  *        description: There is with login credentials
  */
-router.post('/login', async (req, res, next) => {
-  try {
-    const { body } = req;
-    const token = await service.login(body);
-    res.json(token);
-  } catch (err) {
-    next(err);
+router.post(
+  '/login',
+  dataValidator(createUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { body } = req;
+      const token = await service.login(body);
+      res.json(token);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
+
+// Register
+/**
+ * @swagger
+ * /api/v1/auth/register:
+ *  post:
+ *    summary: Register
+ *    description: get token
+ *    tags: [Auth]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *    responses:
+ *      '201':
+ *        description: register
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              example: {message: string}
+ *
+ *      '400':
+ *        description: There is with the data
+ */
+router.post(
+  '/register',
+  dataValidator(createUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { body } = req;
+      const newUser = await service.registerUser(body);
+      res.status(201).json(newUser);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 module.exports = router;
--- services/auth.service.js
+++ services/auth.service.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const boom = require('@hapi/boom');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 
 // Models
 const { models } = require('../db/sequelize');
@@ -34,13 +35,58 @@ class AuthService {
     return user;
   }
 
-  async signtoken(userData) {
+  async signtoken(user) {
     const payload = {
-      sub: userData.id,
-      role: userData.role,
+      sub: user.id,
+      role: user.role,
     };
     const token = jwt.sign(payload, process.env.JWTSECRET);
     return token;
+  }
+
+  async registerUser(data) {
+    await models.User.create(data);
+    await this.sendWelcomeEmail(data.email);
+    return { message: 'registered user' };
+  }
+
+  async sendWelcomeEmail(email) {
+    const user = await this.getUserByEmail(email);
+    const token = await this.signtoken(user);
+    const adminToken = process.env.ADMIN_TOKEN;
+
+    const emailText = `welcome to the world of disney
+    and thank you for joining our wonderful community;
+    This is your token: ${token}. with it you can access get-type endpoints.
+    If you want to try the others, use this admin token: ${adminToken}`;
+
+    const emailHtml = `<h1>welcome to the world of disney</h1>
+    <h4>Thank you for joining our wonderful community</h4>
+    <p>This is your token:</p><p><b>${token}</b></p><p>with it you can access get-type endpoints.<br>
+    If you want to try the others, use this admin token:</p><p><b>${adminToken}</b><p/>`;
+
+    const infoEmail = {
+      from: process.env.NODEMAILER_EMAIL,
+      to: `${email}`,
+      subject: 'Welcome to disney!',
+      text: emailText,
+      html: emailHtml,
+    };
+
+    await this.sendEmail(infoEmail);
+  }
+
+  async sendEmail(infoEMail) {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      secure: true,
+      port: 465,
+      auth: {
+        user: process.env.NODEMAILER_EMAIL,
+        pass: process.env.NODEMAILER_PASSWORD,
+      },
+    });
+    await transporter.sendMail(infoEMail);
   }
 }
 
